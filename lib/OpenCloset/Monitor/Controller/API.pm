dddc040a01f9be28193f8b28ad22be6d8f8f6363
Hyungsuk Hong <aanoaa@gmail.com>
2015-06-01 18:14:15 +0900

    call opencloset API after normal authentication

--- lib/OpenCloset/Monitor/Controller/API.pm
+++ lib/OpenCloset/Monitor/Controller/API.pm
@@ -1,11 +1,11 @@
 package OpenCloset::Monitor::Controller::API;
 use Mojo::Base 'Mojolicious::Controller';
 
-use HTTP::Tiny;
-use MIME::Base64;
+use DateTime;
 use Encode 'decode_utf8';
-
-# PUT /api/orders/:id?status_id=17
+use HTTP::CookieJar;
+use HTTP::Tiny;
+use Path::Tiny;
 
 =head1 METHODS
 
@@ -20,21 +20,38 @@ sub order {
     my $order_id  = $self->param('order_id');
     my $status_id = $self->param('status_id');
 
-    my $opencloset    = $self->app->config->{opencloset};
-    my $authorization = 'Basic '
-        . encode_base64( "$opencloset->{username}:$opencloset->{secret}", '' );
+    my $opencloset = $self->app->config->{opencloset};
+    my $cookie     = path( $opencloset->{cookie} )->touch;
+    my $cookiejar  = HTTP::CookieJar->new->load_cookies( $cookie->lines );
+    my $http       = HTTP::Tiny->new( timeout => 3, cookie_jar => $cookiejar );
 
-    my $http = HTTP::Tiny->new( timeout => 3 );
+    my ($cookies) = $cookiejar->cookies_for( $opencloset->{uri} );
+    my $expires   = $cookies->{expires};
+    my $now       = DateTime->now->epoch;
+    if ( !$expires || $expires < $now ) {
+        my $email    = $opencloset->{email};
+        my $password = $opencloset->{password};
+        my $url      = $opencloset->{uri} . "/login";
+        my $res      = $http->post_form( $url,
+            { email => $email, password => $password, remember => 1 } );
+
+        print "$res->{status} $res->{reason}\n";
+        $cookie->spew( join "\n", $cookiejar->dump_cookies );
+
+        ## 성공일때 응답코드가 302 인데, 이는 실패했을때와 마찬가지이다.
+        unless ( $res->{success} ) {
+            $self->app->log->error("Failed Authentication to Opencloset");
+            $self->app->log->error("$res->{status} $res->{reason}");
+        }
+    }
+
     my $params = $http->www_form_urlencode(
         { id => $order_id, status_id => $status_id } );
-    my $res = $http->request(
-        'PUT',
-        $opencloset->{uri} . "/api/order/$order_id.json?$params",
-        { headers => { Authorization => $authorization } }
-    );
-
+    my $url = $opencloset->{uri} . "/api/order/$order_id.json?$params";
+    my $res = $http->request( 'PUT', $url );
+    return $self->error( 500, { str => 'Failed to update order' } )
+        unless $res->{success};
     $self->render( text => decode_utf8( $res->{content} ) );
 }
-
 
 1;
