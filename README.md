229a00cf0a87bf9fac858f5dacc7d3eeb48e991e
Hyungsuk Hong <aanoaa@gmail.com>
2015-11-11 19:32:46 +0900

    bumpup version

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Dashboard in waiting room
 
 ## Version ##
 
-v0.3.11
+v0.4.0
 
 ## 요구사항 ##
 
--- lib/OpenCloset/Monitor.pm
+++ lib/OpenCloset/Monitor.pm
@@ -6,7 +6,7 @@ use Net::IP::AddrRanges;
 use OpenCloset::Schema;
 use OpenCloset::Monitor::Schema;
 
-use version; our $VERSION = qv("v0.3.11");
+use version; our $VERSION = qv("v0.4.0");
 
 has ranges => sub { Net::IP::AddrRanges->new };
 has DB => sub {
