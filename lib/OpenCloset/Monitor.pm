2754d34789766686990afdb536bc0e267aaaeec8
Hyungsuk Hong <aanoaa@gmail.com>
2016-06-14 18:33:41 +0900

    Bumpup version

--- lib/OpenCloset/Monitor.pm
+++ lib/OpenCloset/Monitor.pm
@@ -7,7 +7,7 @@ use OpenCloset::Monitor::Schema;
 use OpenCloset::Schema;
 use OpenCloset::Monitor::Status;
 
-use version; our $VERSION = qv("v0.7.0");
+use version; our $VERSION = qv("v0.8.0");
 
 has ranges => sub { Net::IP::AddrRanges->new };
 has DB => sub {
