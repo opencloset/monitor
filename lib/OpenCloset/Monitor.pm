66c16fd5811b973cee905d7ce09fd162f17db1eb
Hyungsuk Hong <aanoaa@gmail.com>
2017-04-18 19:03:59 +0900

    Bumpup version

--- lib/OpenCloset/Monitor.pm
+++ lib/OpenCloset/Monitor.pm
@@ -11,7 +11,7 @@ use OpenCloset::Monitor::Schema;
 use OpenCloset::Schema;
 use OpenCloset::Monitor::Status;
 
-use version; our $VERSION = qv("v0.9.4");
+use version; our $VERSION = qv("v0.9.5");
 
 our $PREFIX        = 'opencloset:storage';
 our $REDIS_CHANNEL = 'opencloset:monitor';
