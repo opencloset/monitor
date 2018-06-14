0d21d7bd10a14311458a81bc7153eab8a33096f7
Hyungsuk Hong <aanoaa@gmail.com>
2018-06-14 17:27:30 +0900

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'inline-source-map',
+  entry: {
+    index: [
+      './public/assets/dist/js/common.js',
+      './public/assets/dist/js/index.js',
+      './public/assets/dist/js/default-css.js'
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public', 'assets', 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/assets/dist/'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      _: "underscore"
+    })
+  ]
+};
