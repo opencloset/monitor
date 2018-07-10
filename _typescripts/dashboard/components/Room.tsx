65c1bc3e60d8696d4a7015c916a3b78301001057
Hyungsuk Hong <aanoaa@gmail.com>
2018-07-10 13:02:32 +0900

    Room color by user gender

--- _typescripts/dashboard/components/Room.tsx
+++ _typescripts/dashboard/components/Room.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export interface RoomProps { no: number, name: string };
+export interface RoomProps { no: number, name: string, gender: string };
 
 export class Room extends React.Component<RoomProps, {}> {
   constructor(props: RoomProps) {
@@ -8,8 +8,24 @@ export class Room extends React.Component<RoomProps, {}> {
   }
 
   render() {
+    let tileColor: string;
+    switch (this.props.gender) {
+      case 'male': {
+        tileColor = 'is-info';
+        break;
+      }
+      case 'female': {
+        tileColor = 'is-danger';
+        break;
+      }
+      default: {
+        tileColor = 'is-warning';
+        break;
+      }
+    }
+
     return <div className="tile is-parent">
-      <div className="tile is-child notification is-warning box">
+      <div className={"tile is-child notification box " + tileColor}>
         <p className="subtitle">{this.props.no}</p>
         <p className="title">{this.props.name}</p>
       </div>
